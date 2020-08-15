34b8bb0ddd738f00af633814a5221d2654244822
Mateus Catel <mdasilvacatel@hotmail.com>
2020-08-14 23:10:28 -0300

    add notfoundpage - 404

--- web/src/routes.js
+++ web/src/routes.js
@@ -1,11 +1,21 @@
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Redirect, Switch } from 'react-router-dom';
 import Router from './pages/Router';
+import NotFoundPage from './pages/NotFoundPage';
 
 function Routes() {
     return (
         <BrowserRouter>
-            <Route path="/" exact component={Router} />
+            <Switch>
+                <Route path="/router" exact component={Router} />
+                <Route path="/404" exact component={NotFoundPage} />
+
+                <Route path="/" exact>
+                    <Redirect to="/router" />
+                </Route>
+
+                <Redirect to="/404" />
+            </Switch>
         </BrowserRouter>
     );
 }
