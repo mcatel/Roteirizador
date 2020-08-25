757b30ce4ec344b40e5b4315a6900f5dcdb52902
Mateus Catel <mdasilvacatel@hotmail.com>
2020-08-25 10:35:17 -0300

    add transactions

--- server/src/controllers/RouteController.js
+++ server/src/controllers/RouteController.js
@@ -1,24 +1,42 @@
 const Route = require('../models/Route');
 const Stop = require('../models/Stop');
+const connection = require('../database/connection');
 
 module.exports = {
     async store(req, res) {
         const { coordinates } = req.body;
-        const route = await Route.create();
 
-        coordinates.forEach(async coordinate => {
-            await Stop.create({
-                stop_point: {
-                    type: "point",
-                    coordinates: [
-                        coordinate.latitude,
-                        coordinate.longitude
-                    ]
-                },
-                route_id: route.id
+        try {
+
+            await connection.transaction(async (t) => {
+                const route = await Route.create(null, { transaction: t });
+
+                coordinates.forEach(async coordinate => {
+
+                    await connection.transaction(async (t) => {
+                        await Stop.create({
+                            stop_point: {
+                                type: "point",
+                                coordinates: [
+                                    coordinate.latitude,
+                                    coordinate.longitude
+                                ]
+                            },
+                            route_id: route.id
+                        }, {
+                            transaction: t
+                        });
+                    });
+                });
+
+                return res.json(route);
             });
-        });
 
-        return res.json(route);
+        } catch (error) {
+
+            return res.status(400).json({
+                error: "Unexpected error while creating new route"
+            });
+        }
     }
 }
