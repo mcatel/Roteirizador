ec9954f4938cb55e902b32bef607b42658e30c86
catel <mdasilvacatel@hotmail.com>
2022-06-28 23:51:14 -0300

    Update database config to use heroku-postgres

--- server/config/config.js
+++ server/config/config.js
@@ -8,7 +8,10 @@ const getDatabaseConfig = () => {
       use_env_variable: 'DATABASE_URL',
       dialect: 'postgres',
       dialectOptions: {
-        ssl: true,
+        ssl: {
+          require: true,
+          rejectUnauthorized: false,
+        },
       },
     };
   }
