e577a1d8eb36b8d246aa931062d21ce44a965bea
catel <mdasilvacatel@hotmail.com>
2022-06-28 22:44:08 -0300

    Update database config to use heroku-postgres

--- server/src/database/connection.js
+++ server/src/database/connection.js
@@ -1,9 +1,18 @@
 import { Sequelize } from 'sequelize';
-import config from './config';
 import Route from '../models/Route';
 import Stop from '../models/Stop';
 
-const connection = new Sequelize(config);
+require('dotenv').config({
+  path: process.env.NODE_ENV === 'test' ? '.env.test' : '.env',
+});
+
+const connection = new Sequelize(process.env.DATABASE_URL, {
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true,
+  },
+});
 
 const models = {
   Route: Route.init(connection),
